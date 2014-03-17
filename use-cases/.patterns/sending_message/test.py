6649f90917308452e0ec6b9a65bfaa2e10ec99ee
Jacob Sparre Andersen <jsa@adaheads.com>
2014-03-17 09:24:55 +0000

    Test case patterns: Send and check for message.

--- use-cases/.patterns/sending_message/test.py
+++ use-cases/.patterns/sending_message/test.py
@@ -1,7 +1,7 @@
             self.Step (Message = "Receptionist-N     ->> Klient-N          [genvej: send-besked]")
-            self.Step (Message = "Klient-N           ->> Call-Flow-Control [send <besked> til <modtagerliste>]")
+            self.Receptionist_Send_Message ()
             self.Step (Message = "Call-Flow-Control  ->> Message-Spool     [send <besked> til <modtagerliste>]")
-            self.Step (Message = "Message-Spool      ->> Medarbejder       [<besked>", note = "muligvis\ntil flere]")
+            self.Callee_Checks_For_Message ()
             self.Step (Message = "Receptionist-N    <<-  Klient-N          [ryddet 'send besked'-dialog]")
 
             self.Postprocessing ()
