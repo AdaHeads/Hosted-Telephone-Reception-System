fa01d2553deca14ae354687044a30b923fa2839e
Jacob Sparre Andersen <jsa@adaheads.com>
2014-03-24 09:30:08 +0000

    Correction to match 'send_message' test class.

--- use-cases/.patterns/record_name/test.py
+++ use-cases/.patterns/record_name/test.py
@@ -7,6 +7,6 @@ from config       import queued_reception as Reception
 class Sequence_Diagram (Test_Case):
     def test_Run (self):
         try:
-            self.Preconditions (Reception = Reception)
+            self.Preconditions ()
 
             self.Step (Message = "Receptionist-N     ->> Klient-N          [taster: navn]")
